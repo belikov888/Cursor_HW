35bb153645f2dde8335b60e23be2705de61a9359
Alex <belikov.wood@gmail.com>
2022-06-02 13:53:01 +0300

    modified homework5

--- HW5_Arrays/main.js
+++ HW5_Arrays/main.js
@@ -1,30 +1,39 @@
-const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
+const STUDENTS = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 
-const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
+const THEMES = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 
-const marks = [4, 5, 5, 3, 4, 5];
+const MARKS = [4, 5, 5, 3, 4, 5];
 
-function getPairs(students) {
-    const arr = [
-        [students[0], students[2]], 
-        [students[1], students[3]], 
-        [students[4], students[5]]
-    ];
+function getPairs(STUDENTS) {
+    const arr = [];
+    const manArr = [];
+    const girlArr = [];
+    for(let i = 0; i < STUDENTS.length; i++) {
+        if(STUDENTS[i].substring(STUDENTS[i].length - 1) !== 'а') {
+            manArr.push(STUDENTS[i]);
+        }
+        else {
+            girlArr.push(STUDENTS[i]);
+        }    
+    }
+    for(let i = 0; i < manArr.length; i++) {
+        arr.push([manArr[i], girlArr[i]]);
+    }
     return arr;
 }
 
 function getProject(pairs) {
     const arr = [];
     for(let i = 0; pairs[i]; i++) {
-        arr.push([pairs[i].join(' i '), themes[i]]);
+        arr.push([pairs[i].join(' i '), THEMES[i]]);
     }
     return arr;
 }
 
-function getGrade(students) {
+function getGrade(STUDENTS) {
     const arr = [];
-    for(let i = 0; students[i]; i++) {
-        arr.push([students[i], marks[i]]);
+    for(let i = 0; STUDENTS[i]; i++) {
+        arr.push([STUDENTS[i], MARKS[i]]);
     }
     return arr;
 }
@@ -32,21 +41,20 @@ function getGrade(students) {
 function getGradeRandom(projects) { 
     const arr = [];
     for(let i = 0; projects[i]; i++) {
-        let random = Math.random() * 4 + 1;
-        random = random.toFixed();
-        arr.push(projects[i].slice());
+        let random = Math.floor(Math.random() * 5 + 1);
+        arr.push(projects[i].slice());  
         arr[i].push(random);
     }
     return arr;
 }
 
-const pairs = getPairs(students);
+const pairs = getPairs(STUDENTS);
 console.log(pairs);
 
 const projects = getProject(pairs);
 console.log(projects);
 
-const grade = getGrade(students);
+const grade = getGrade(STUDENTS);
 console.log(grade);
 
 const gradeRandom = getGradeRandom(projects);
