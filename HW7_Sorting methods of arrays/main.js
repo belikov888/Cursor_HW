6c5e500fdbf0237271afb3c909a4a9597cd82d86
Alex <belikov.wood@gmail.com>
2022-06-30 14:53:29 +0300

    fixed homework 7

--- HW7_Sorting methods of arrays/main.js
+++ HW7_Sorting methods of arrays/main.js
@@ -1,10 +1,7 @@
 function getRandomArray(length, min, max) {
-    let random;
-    let result = [];
+    const result = [];
     for(let i = 0; i < length; i++) {
-        random = Math.random() * (max - min) + min;
-        random = random.toFixed();
-        result.push(random);
+        result.push(Math.floor(Math.random() * (max - min) + min));
     }
     return result;
 
@@ -13,19 +10,15 @@ function getRandomArray(length, min, max) {
 console.log(`Функція, яка повертає масив випадкових цілих чисел: ${getRandomArray(10, 1, 100)}`);
 
 function getModa(...numbers) { 
-    let arr = [];
-    let newArr = [];
+    const newArr = [];
     for(let i = 0; i < numbers.length; i++) {
+        const arr = numbers.filter(number => number == numbers[i]);
         if((numbers[i] ^ 0) !== numbers[i]) {
             continue
         }
         let n = numbers.indexOf(numbers[i]);
-        if(n < i){
-        } else {
-        arr = numbers.filter(number => number == numbers[i]);
-        if(arr.length > 1) {
-            newArr.push(arr);
-        }
+        if(!(n < i) && arr.length > 1){
+                newArr.push(arr);
         } 
     }
     let arrW = [[0]];
@@ -37,6 +30,7 @@ function getModa(...numbers) {
         else if(newArr[i].length == arrW[0].length) {
             arrW.push(newArr[i]);
         }
+
     }
     let result = arrW.map(item => item.slice(0, 1).join());
     return result;
@@ -44,16 +38,13 @@ function getModa(...numbers) {
 
 console.log(`Функція, яка вираховує моду всіх переданих в неї аргументів: ${getModa(1, 1.2, 3.2, 3, 2, 4, 1, 6, 1, 7, 3, 7, 3, 5)}`);   
 
-function getAverage(...numbers) {
-    let sum = 0;
+function getAverage(...numbers) { 
     let result;
-    for(let i = 0; i < numbers.length; i++) {
-        if((numbers[i] ^ 0) !== numbers[i]) {
-            continue;
-        }
-        sum = sum + +numbers[i];
-    }
-    result = sum / numbers.length;
+    let arr = [];
+    const initialValue = 0;
+    arr = numbers.filter(number => (number ^ 0) == number);
+    const sum = arr.reduce((a, b) => a + b, initialValue);  
+    result = sum / arr.length;
     return result.toFixed(2);
 }
 
@@ -65,11 +56,7 @@ function getMedian(...numbers) {
     let arr;
     let result = 0;
     arr = numbers.filter(iteam => (iteam ^ 0) === iteam);
-    arr.sort((a, b) => {
-        if (a > b) return 1;
-        if (a == b) return 0;
-        if (a < b) return -1;
-    });
+    arr.sort((a, b) => a - b);
     if(arr.length % 2 == 0) {
         result = ((arr[arr.length / 2] + arr[arr.length / 2 - 1]) / 2);
     }
@@ -83,91 +70,66 @@ function getMedian(...numbers) {
 console.log(`Функція, яка рахує медіану всіх переданих в неї аргументів: ${getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)}`);
 
 function filterEvenNumbers(...numbers) {
-    let evenNumbers = numbers.filter(iteam => iteam % 2 !== 0);
-    return evenNumbers;
+    return numbers.filter(iteam => iteam % 2 !== 0);;
 }
 
 console.log(`Функція, яка фільтрує парні числа передані як аргументи функції: ${filterEvenNumbers(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)}`);
 
 function countPositiveNumbers(...numbers) {
-    let arr = numbers.filter(iteam => iteam > 0);
-    return arr.length;
+    return numbers.filter(iteam => iteam > 0).length;
 }
 
 console.log(`Функція, яка порахує кількість чисел більших 0: ${countPositiveNumbers(1, -2, 3, -4, -5, 6, 6)}`);
 
 function getDividedByFive(...numbers) {
-    let arr = numbers.filter(iteam => ((iteam / 5) ^ 0) ===  (iteam / 5));
-    return arr;
+    return numbers.filter(num => num % 5 === 0);
+    
 }
 
 console.log(`Функція, яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5: ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 5, 77, 57, 87, 23, 2, 56, 3, 2)}`);
 
 function replaceBadWords(string) { 
     const BADWORDS = ['shit', 'fuck'];
-    let arr = string.split(' ');
-    let indexWord;
-    for(let i = 0; i < BADWORDS.length; i++) {
-        let amonthStars = BADWORDS[i].length;
-        for(let y = 0; y < arr.length; y++) {
-            indexWord = arr[y].indexOf(BADWORDS[i]);
-            let lengthStar = star => {
-                let result = '';
-                while(star > 0) {
-                    star = star - 1;
-                    result = result + '*';
-                }
-                return result;
-            }
-            if(indexWord !== -1) {
-                arr[y] = arr[y].split('');
-                if(indexWord === 0) {
-                    arr[y].splice(indexWord, indexWord + amonthStars, lengthStar(amonthStars));
-                }
-                else {
-                    arr[y].splice(indexWord, indexWord + amonthStars - 1, lengthStar(amonthStars));
-                }
-                arr[y] = arr[y].join('');
-            }
+    for (let i = 0; i < BADWORDS.length; i++) {
+        while (string.includes(BADWORDS[i])) {
+            string = string.replace(BADWORDS[i], '*'.repeat(BADWORDS[i].length))
         }
-    }  
-    return arr.join(' ');  
+    }
+    return string
 }
 
 console.log(`Функція, яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*): ${replaceBadWords("Are you fucking kidding?")}`);
 
 function divideByThree(word) { 
-    let arr = word.toLowerCase().split(' ').join('');
-    let result = [];
-    if(arr.length > 3) {
-        let a = 0;
-        while(a < arr.length) {
-            let newArr = arr.substring(a, a + 3);
-            result.push(newArr.slice());
-            a = a + 3;
-        }
-    }
-    else {
+    const arr = word.toLowerCase().split(' ').join('');
+    const result = [];
+    if(arr.length <= 3) {
         return word;
+    } 
+    let a = 0;
+    while(a < arr.length) {
+        let newArr = arr.substring(a, a + 3);
+        result.push(newArr.slice());
+        a = a + 3;
     }
     return result;
 }
 
 console.log(`Функція, яка розбиває кожне слово на умовні склади по 3 букви: ${divideByThree("Commander")}`);
 
-function generateCombinations(word) {
-    let count = word.length;
-    let arr = word.split('');
-    let result = [];
-    while(count > 0) {
-        for(let i = 0; i < word.length - 1; i++) {
-            let newArr = arr.splice(i, 1);
-            arr.splice(i + 1, 0, newArr.join(''));
-            result.push(arr.slice().join(''));
+function generateCombinations(word) { 
+    const result = [];
+        if (word.length < 2) return [word];
+            
+        for (let i = 0; i < word.length; i++) {
+            let getCombination = generateCombinations(word.slice(0, i) + word.slice(i+1, word.length));
+            
+            for (n = 0; n < getCombination.length; n++) {
+                result.push(word[i] + getCombination[n]);
+            }
         }
-        count--;
+    
+        return result.filter((vallue, index, arr) => arr.indexOf(vallue) === index)
     }
-    return result;
-}
 
 console.log(`Функція, яка видасть всі можливі перестановки(унікальні, без повторень) букв в слові: ${generateCombinations("abcd")}`);
